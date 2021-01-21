5a69df185edbb25469010b2128cfb488316b8121
Kazuhiko Okumura <engkokmr@gmail.com>
2021-01-21 22:28:43 +0900

    シリアライズ方法をJSONにしたことでLocalDateTime型のシリアライズでエラーが出るためStringで保管するように変更しました。

--- src/main/java/net/kazokmr/study/hasession/Reception.java
+++ src/main/java/net/kazokmr/study/hasession/Reception.java
@@ -1,5 +1,9 @@
 package net.kazokmr.study.hasession;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
@@ -14,8 +18,8 @@ public class Reception implements Serializable {
     private static final long serialVersionUID = -3101986789734320497L;
 
     private String host;
-    private LocalDateTime receptionAt;
     private String message;
+    private String receptionAt;
 
     public String getMessage() {
         return message;
@@ -34,10 +38,10 @@ public class Reception implements Serializable {
     }
 
     public String getReceptionAt() {
-        return receptionAt == null ? null : DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(receptionAt);
+        return receptionAt;
     }
 
-    public void setReceptionAt(LocalDateTime receptionAt) {
-        this.receptionAt = receptionAt;
+    public void setReceptionAt() {
+        this.receptionAt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now());
     }
 }
